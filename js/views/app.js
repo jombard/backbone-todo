bfd19ac635b5ee14ff349fd3fbaba29194a71885
Jon Lombard <jombard@hotmail.com>
2015-12-14 20:49:47 +0000

    Correct sytax errors

--- js/views/app.js
+++ js/views/app.js
@@ -8,7 +8,7 @@ app.AppView = Backbone.View.extend({
 
   // Instead of generating a new element, bind to the existing skeleton of
   // the App already present in the HTML.
-  el: '#todoapp',
+  el: '.todoapp',
 
   // Our template for the line of statistics at the bottom of the app.
   statsTemplate: _.template($('#stats-template').html()),
@@ -16,19 +16,19 @@ app.AppView = Backbone.View.extend({
   // New
   // Delegated events for creating new items, and clearing completed ones.
   events: {
-    'keypress #new-todo': 'createOnEnter',
-    'click #clear-completed': 'clearCompleted',
-    'click #toggle-all': 'toggleAllComplete'
+    'keypress .new-todo': 'createOnEnter',
+    'click .clear-completed': 'clearCompleted',
+    'click .toggle-all': 'toggleAllComplete'
   },
 
   // At initialization we bind to the relevant events on the `Todos`
   // collection, when items are added or changed. Kick things off by
   // loading any preexisting todos that might be saved in *localStorage*.
   initialize: function() {
-    this.allCheckbox = this.$('#toggle-all')[0];
-    this.$input = this.$('#new-todo');
-    this.$footer = this.$('#footer');
-    this.$main = this.$('#main');
+    this.allCheckbox = this.$('.toggle-all')[0];
+    this.$input = this.$('.new-todo');
+    this.$footer = this.$('.footer');
+    this.$main = this.$('.main');
 
     this.listenTo(app.Todos, 'add', this.addOne);
     this.listenTo(app.Todos, 'reset', this.addAll);
@@ -75,12 +75,12 @@ app.AppView = Backbone.View.extend({
     var view = new app.TodoView({
       model: todo
     });
-    $('#todo-list').append(view.render().el);
+    $('.todo-list').append(view.render().el);
   },
 
   // Add all items in the **Todos** collection at once.
   addAll: function() {
-    this.$('#todo-list').html('');
+    this.$('.todo-list').html('');
     app.Todos.each(this.addOne, this);
   },
 
